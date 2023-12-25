78cf10a03104704107ffe46942020846d6997222
Jake Gill <jake.w.g21@gmail.com>
2023-12-25 12:47:50 -0500

    Calendar navigation

--- client/src/pages/Barber/Schedule/Schedule.jsx
+++ client/src/pages/Barber/Schedule/Schedule.jsx
@@ -3,55 +3,93 @@ import "./Schedule.css";
 import { useState } from "react";
 
 export default function Schedule() {
-  const [activeDay, setActiveDay] = useState(new Date().getDay());
+	const [activeDay, setActiveDay] = useState(new Date().getDay());
+	const [currentDate, setCurrentDate] = useState(new Date());
 
-  const getWeekDates = () => {
-    const now = new Date();
-    const dayOfWeek = now.getDay(); // Sunday - 0, Monday - 1, etc.
-    const currentMonth = now.toLocaleString("default", { month: "long" });
-    const currentYear = now.getFullYear();
-    const currentDate = now.getDate();
-    let weekDates = [];
+	const getWeekDates = (date) => {
+		const dayOfWeek = date.getDay();
+		const currentMonth = date.toLocaleString("default", { month: "long" });
+		const currentYear = date.getFullYear();
+		const currentDate = date.getDate();
+		let weekDates = [];
 
-    for (let i = 0; i < 7; i++) {
-      const date = new Date(now);
-      date.setDate(currentDate - dayOfWeek + i);
-      weekDates.push(date.getDate());
-    }
+		for (let i = 0; i < 7; i++) {
+			const newDate = new Date(date);
+			newDate.setDate(currentDate - dayOfWeek + i);
+			weekDates.push(newDate.getDate());
+		}
 
-    return { currentMonth, currentYear, weekDates };
-  };
+		return { currentMonth, currentYear, weekDates };
+	};
 
-  const { currentMonth, currentYear, weekDates } = getWeekDates();
-  const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+	const { currentMonth, currentYear, weekDates } = getWeekDates(currentDate);
+	const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-  const handleDayClick = (dayIndex) => {
-    setActiveDay(dayIndex);
-  };
+	const handleDayClick = (dayIndex) => {
+		setActiveDay(dayIndex);
+	};
 
-  return (
-    <>
-      <section className="schedule-container">
-        <h2 className="schedule-title">Schedule</h2>
-        <div className="calendar">
-          <div className="month-year">{`${currentMonth.toUpperCase()} ${currentYear}`}</div>
-          <div className="week-days">
-            {daysOfWeek.map((day, index) => (
-              <div
-                key={day}
-                className="day"
-                onClick={() => handleDayClick(index)}
-              >
-                <div>{day}</div>
-                <div className={`${index === activeDay ? "active" : ""}`}>
-                  {weekDates[index]}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-      <NavbarBarber />
-    </>
-  );
+	const handlePrevWeek = () => {
+		const newDate = new Date(currentDate);
+		newDate.setDate(newDate.getDate() - 7);
+		setCurrentDate(newDate);
+	};
+
+	const handleNextWeek = () => {
+		const newDate = new Date(currentDate);
+		newDate.setDate(newDate.getDate() + 7);
+		setCurrentDate(newDate);
+	};
+
+	return (
+		<>
+			<section className='schedule-container'>
+				<h2 className='schedule-title'>Schedule</h2>
+				<div className='calendar'>
+					<div className='month-year'>{`${currentMonth.toUpperCase()} ${currentYear}`}</div>
+					<div className='week-days'>
+						{daysOfWeek.map((day, index) => (
+							<div
+								key={day}
+								className='day'
+								onClick={() => handleDayClick(index)}
+							>
+								<div>{day}</div>
+								<div className={`${index === activeDay ? "active" : ""}`}>
+									{weekDates[index]}
+								</div>
+							</div>
+						))}
+					</div>
+					<div className='calendar-navigate-container'>
+						<svg
+							onClick={handlePrevWeek}
+							xmlns='http://www.w3.org/2000/svg'
+							height='24'
+							width='21'
+							viewBox='0 0 448 512'
+						>
+							<path
+								fill='var(--lightest)'
+								d='M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z'
+							/>
+						</svg>
+						<svg
+							onClick={handleNextWeek}
+							xmlns='http://www.w3.org/2000/svg'
+							height='24'
+							width='21'
+							viewBox='0 0 448 512'
+						>
+							<path
+								fill='var(--lightest)'
+								d='M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z'
+							/>
+						</svg>
+					</div>
+				</div>
+			</section>
+			<NavbarBarber />
+		</>
+	);
 }
