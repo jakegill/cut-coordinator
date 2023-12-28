b748a91b1227368e2c0b20d4bd3ac43749a6a4c5
Jake Gill <jake.w.g21@gmail.com>
2023-12-28 13:51:12 -0500

    Update server.js to use environment variable for port

--- api/server.js
+++ api/server.js
@@ -24,7 +24,9 @@ mongoose
 
 const __dirname = path.resolve();
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT, () =>
+	console.log(`Server running on port ${process.env.PORT}`)
+);
 
 // Routes
 app.use("/api/auth", auth);
